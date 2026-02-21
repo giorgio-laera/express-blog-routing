712c5d6f2c96b6aa038044ddc01cdbd77e396914
giorgio laera <laeragiorg@gmail.com>
2026-02-21 12:16:35 +0100

    ticket

---
+++ controllers/postsController.js
@@ -0,0 +1,44 @@
+const posts = require('../data/posts')
+console.log(posts);
+function index (req,res) {
+    let result = posts;
+
+     res.json(result);
+
+}
+
+function show(req,res) {
+
+    res.json(posts.filter((post) => post.id == req.params.id));
+}
+
+
+function store(req,res) {
+
+    res.send('hai chiesto di CREARE un nuovo elemento');
+}
+
+function update(req,res) {
+
+    res.send(`hai chiesto di MODIFICARE un INTERO ELEMENTO ${req.params.id}`);
+}
+
+function modify(req,res) {
+
+    res.send(`hai richiesto di MODIFICARE (parzialmente) un elemento ${req.params.id} `);
+}
+
+function destroy(req,res) {
+
+    res.send(`hai chiesto di ELIMINARE un elemento ${req.params.id} `);
+}
+const postsController = {
+    index,
+    show,
+    store,
+    update,
+    modify,
+    destroy
+};
+
+module.export = postsController;
