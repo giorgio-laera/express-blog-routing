cef7f613e95288e7be1167382bd40b996bffeeee
giorgio laera <laeragiorg@gmail.com>
2026-02-22 11:23:28 +0100

    aggiunti contolli a show e aggiunta img

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,44 +1,59 @@
 const posts = require('../data/posts')
-console.log('posts',posts);
-function index (req,res) {
-    let result = posts;
 
-     res.json(result);
+function index(req, res) {
+	let result = posts;
+
+	res.json(result);
 
 }
 
-function show(req,res) {
+function show(req, res) {
+	const id = Number(req.params.id);
+	let result =[];
 
-    res.json(posts.filter((post) => post.id == req.params.id));
+	if (!isNaN(id)) {
+		result = posts.filter((post) => post.id == req.params.id);
+
+		res.status(200)
+	} 
+	if( result.length == 0 || isNaN(id)){
+		res.status(404)
+		result = {
+			error: "Not Found",
+			message: "Pizza non trovata"
+		}}
+	
+
+	res.json(result)
 }
 
 
-function store(req,res) {
+function store(req, res) {
 
-    res.send('hai chiesto di CREARE un nuovo elemento');
+	res.send('hai chiesto di CREARE un nuovo elemento');
 }
 
-function update(req,res) {
+function update(req, res) {
 
-    res.send(`hai chiesto di MODIFICARE un INTERO ELEMENTO ${req.params.id}`);
+	res.send(`hai chiesto di MODIFICARE un INTERO ELEMENTO ${req.params.id}`);
 }
 
-function modify(req,res) {
+function modify(req, res) {
 
-    res.send(`hai richiesto di MODIFICARE (parzialmente) un elemento ${req.params.id} `);
+	res.send(`hai richiesto di MODIFICARE (parzialmente) un elemento ${req.params.id} `);
 }
 
-function destroy(req,res) {
+function destroy(req, res) {
 
-    res.send(`hai chiesto di ELIMINARE un elemento ${req.params.id} `);
+	res.send(`hai chiesto di ELIMINARE un elemento ${req.params.id} `);
 }
 const postsController = {
-    index,
-    show,
-    store,
-    update,
-    modify,
-    destroy
+	index,
+	show,
+	store,
+	update,
+	modify,
+	destroy
 };
 
 module.exports = postsController;
--- routers/posts.js
+++ routers/posts.js
@@ -4,7 +4,7 @@ const router =express.Router();
 const postsController =require('../controllers/postsController');
 //console.log(router)
 
-console.log(postsController);
+//console.log(postsController);
 
 // const posts = [
 //   {
@@ -60,11 +60,11 @@ router.get('/', postsController.index)
 //Show(cRud) visualizzare singolo elemento
 router.get('/:id', postsController.show)
 //Store(Crud) per aggiungere un elemento
-//router.post('/', postsController.store)
+router.post('/', postsController.store)
 //Update(crUd) serve per MODIFICARE un INTERO ELEMENTO
-//router.put('/:id', postsController.update)
+router.put('/:id', postsController.update)
 //Modify (crUd) serve  MODIFICARE (parzialmente) un elemento
-//router.patch('/:id', postsController.modify)
+router.patch('/:id', postsController.modify)
 // Destroy (cruD) serve per ELIMINARE un elemento
 router.delete('/:id', postsController.destroy)
 
